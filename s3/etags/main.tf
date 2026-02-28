43e6635ee34944f9927c539863e6b84387783c61
isaacbenhale <isaacbenhale@gmail.com>
2026-02-28 13:28:22 +0000

    Using terraform to upload a file to s3

---
+++ s3/etags/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.34.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+
+resource "aws_s3_object" "object" {
+  bucket = "isaac-tf-bucket2"
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("myfile.txt")
+}
